5dc4b580ae51c46fce18c816c7d72b46bf68d58f
Spiraline <snowbaepbird@gmail.com>
2022-03-11 14:21:13 +0900

    Add tqdm option in trace dist viz

--- util/trace_dist_viz.py
+++ util/trace_dist_viz.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', '-i', required=True, type=str)
     parser.add_argument('--page_size', '-p', default='8K', type=str)
+    parser.add_argument('--tqdm', '-t', action='store_true')
 
     makedirs('res', exist_ok=True)
 
@@ -35,30 +36,42 @@ if __name__ == "__main__":
     page_size = parseIntPrefix(args.page_size)
 
     read_ratio_file = open('res/rw_ratio.csv', 'w')
+    read_ratio_file.write('trace,read_num,write_num,write_ratio,max_LBA,LBA_in_use,utilization\n')
 
     for trace_path in trace_list:
         LBA_list = []
         read_num = 0
         write_num = 0
+        LBA_in_use = set()
         trace_name = basename(trace_path).split('.')[0]
         
         with open(trace_path, 'r') as trace_file:
             req_num = int(trace_file.readline())
             trace_max_addr = int(trace_file.readline())
-            progress = tqdm(total=req_num)
-            progress.set_description(trace_name)
+            if args.tqdm:
+                progress = tqdm(total=req_num)
+                progress.set_description(trace_name)
             for req in range(req_num):
                 op, LBAs = parseReq(trace_file.readline(), page_size)
                 if op == 'write':
                     write_num += 1
                     for LBA in LBAs:
+                        LBA_in_use.add(LBA)
                         LBA_list.append(LBA)
                 else:
                     read_num += 1
-                progress.update(1)
+                if args.tqdm:
+                    progress.update(1)
+            if args.tqdm:
+                progress.close()
 
+        max_LBA = trace_max_addr // page_size
+        utilization = len(LBA_in_use) / max_LBA * 100
         write_ratio = write_num / (read_num + write_num) * 100
-        read_ratio_file.write('%s,%d,%d,%f' % (trace_name, read_num, write_num, write_ratio))
+
+        read_ratio_file.write('%s,%d,%d,%f,%d,%d,%f\n'
+            % (trace_name, read_num, write_num, write_ratio,
+                max_LBA,len(LBA_in_use),utilization))
 
         fig, ax = plt.subplots()
         plt.hist(LBA_list, bins=500)
